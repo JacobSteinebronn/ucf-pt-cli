1d4ad70c9998b50279214ab73e7aaa480c715710
Jacob S <jakesteinebronn@gmail.com>
2024-01-25 14:03:13 -0500

    Implement printfile

--- printfile/printfile.py
+++ printfile/printfile.py
@@ -1,8 +1,12 @@
 import argparse
+import os
+import subprocess
 
 def attach_parser(subparsers):
     cur = subparsers.add_parser("printfile", help="Formats and prints a source file")
     cur.add_argument("FILE", help="The source file to print")
 
 def exec(args):
-    print("PRINTFILE", args)
+    dirr = os.path.dirname(os.path.abspath(__file__))
+    script = os.path.join(dirr, 'printfile')
+    subprocess.call(f"bash {script} {args.FILE}".split())
